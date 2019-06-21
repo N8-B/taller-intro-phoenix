c3e2d11a2ddd63fc9c724cff6b4ea25746eb6351
E053689 <nathan.adam@bbvaitspain.com>
2019-06-21 20:43:48 +0200

    Adds slides

---
+++ theme.js
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    text: '#FFF',
+    background: '#1C1E23',
+    link: '#45C3EC',
+    code: '#45C3EC'
+   },
+  css: {
+    textAlign: 'left',
+    fontSize: '1.5em',
+    '@media screen and (min-width:64em)': {
+      fontSize: '2em'
+    }
+  },
+  Slide: {
+    '& > div': {
+      minWidth: '80vw',
+      minHeight: '60vh'
+    }
+  }
+}
